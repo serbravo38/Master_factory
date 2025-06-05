f54d7a383f07fca53cac8aaac9772397f80d3b13
serbravo38 <164573839+serbravo38@users.noreply.github.com>
2025-06-05 15:46:12 -0400

    Se actualiza base de datos a la 3NF

--- factory/models.py
+++ factory/models.py
@@ -1,15 +1,5 @@
 from django.db import models
 
-# Create your models here.
-class Solicitud(models.Model):
-    codigo=models.CharField(primary_key=True,max_length=6)
-    nombre=models.CharField(max_length=50)
-    cantidad=models.PositiveSmallIntegerField()
-
-    def __str__(self):
-        texto = "{0} ({1})"
-        return texto.format(self.nombre, self.cantidad)
-    
 class Cliente(models.Model):
     codigo = models.CharField(primary_key=True, max_length=6)
     nombre = models.CharField(max_length=50)
@@ -19,3 +9,13 @@ class Cliente(models.Model):
     def __str__(self):
         return f"{self.correo} ({self.nombre})"
 
+class Solicitud(models.Model):
+    codigo = models.CharField(primary_key=True, max_length=6)
+    nombre = models.CharField(max_length=50)  # Si es el nombre de la solicitud, está bien.
+    cantidad = models.PositiveSmallIntegerField()
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, default=1)
+
+
+    def __str__(self):
+        texto = "{0} ({1})"
+        return texto.format(self.nombre, self.cantidad)
